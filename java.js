d001acd1d2064c74a5406db82d790a867bd4408e
Daniel <danielbahia12@yahoo.com.br>
2021-08-11 18:17:54 -0300

    alterei o código arrumando o erro nos numeors pares e multiplos de 5

--- java.js
+++ java.js
@@ -7,7 +7,7 @@ let s = "Snap"
 let separate = ", "
 function snapCrackle(maxValue) {
    for (let i=1; i <= maxValue; i++) {
-   if (i % 2 !== 2 && i % 5 === 0) { // o resto da divisao por 2 diferente de 2 E o resto da divisao por 5 diferente de 0
+   if (i % 2 !== 0 && i % 5 === 0) { // o resto da divisao por 2 diferente de 2 E o resto da divisao por 5 diferente de 0
     novaLista += sc
    } else if (i % 2 !== 0) { // o resto da divisão do i por 2 tem que ser diferente de 0 (IMPAR)
     novaLista += s
